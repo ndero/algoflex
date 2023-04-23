35b6e984cc0c6844457f94b1fbd72ff81c9af519
ndero <jnderoh@gmail.com>
2023-04-24 02:05:53 +0300

    ft: add pass modal

--- src/algoflex/attempt.py
+++ src/algoflex/attempt.py
@@ -1,12 +1,28 @@
 from textual.app import App
-from textual.widgets import TextArea, Button
+from textual.widgets import TextArea, Button, Markdown
 from home import ProblemScreen
 from textual.containers import VerticalGroup, HorizontalGroup
 from textual.screen import ModalScreen
 
 
 class PassScreen(ModalScreen):
-    pass
+    PASS_MD = """
+    Passed in 12 minutes and 16 seconds!
+    Awesome!!
+    """
+    BINDINGS = [("escape", "dismiss", "Dismiss")]
+
+    DEFAULT_CSS = """
+    PassScreen {
+        align: center middle;
+    }
+    Markdown {
+        width: 45;
+    }
+    """
+
+    def compose(self):
+        yield Markdown(self.PASS_MD)
 
 
 class AttemptScreen(App):
@@ -14,6 +30,7 @@ class AttemptScreen(App):
 def solution():
     # your solution here
 """
+    BINDINGS = [("s", "show_modal", "Editor")]
 
     def compose(self):
         with HorizontalGroup():
@@ -25,6 +42,9 @@ def solution():
                 with HorizontalGroup():
                     yield Button("submit")
                     yield Button("cancel")
+
+    def action_show_modal(self):
+        self.push_screen(PassScreen())
 
 
 if __name__ == "__main__":
