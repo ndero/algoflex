3e0787fd86702c9ed80fcd15a51cd3d2cd4a2e79
ndero <jnderoh@gmail.com>
2023-05-11 22:49:59 +0300

    ch: push usercode from attempt screen

--- src/algoflex/attempt.py
+++ src/algoflex/attempt.py
@@ -1,52 +1,19 @@
 from textual.app import App
 from textual.widgets import TextArea, Button, Markdown, Footer
 from home import ProblemScreen
+from result import ResultModal
 from textual.containers import VerticalGroup, HorizontalGroup
 from textual.screen import ModalScreen, Screen
 
 
-class PassScreen(ModalScreen):
-    PASS_MD = """
-    Passed in 12 minutes and 16 seconds!
-    Awesome!!
-    """
-    BINDINGS = [("escape", "dismiss", "Dismiss")]
-
-    DEFAULT_CSS = """
-    PassScreen {
-        align: center middle;
-    }
-    Markdown {
-        width: 45;
-    }
-    """
-
-    def compose(self):
-        yield Markdown(self.PASS_MD)
-
-
-class FailScreen(ModalScreen):
-    FAIL_MD = """
-    Failed,
-    """
-    BINDINGS = [("escape", "dismiss", "Dismiss")]
-    DEFAULT_CSS = """
-    FailScreen {
-        align: center middle;
-    }
-    Markdown {
-        width: 50;
-    }
-    """
-
-    def compose(self):
-        yield Markdown(self.FAIL_MD)
-
-
 class AttemptScreen(Screen):
-    DEFAULT_TEXT = """\
-def solution():
-    # your solution here
+    DEFAULT_CODE = """\
+def solution(nums, target):
+    lookup = {}
+    for i, num in enumerate(nums):
+        if target - num in lookup:
+            return [lookup[target - num], i]
+        lookup[num] = i
 """
     BINDINGS = [("s", "show_modal", "Editor")]
 
@@ -55,7 +22,7 @@ def solution():
             yield ProblemScreen()
             with VerticalGroup():
                 yield TextArea(
-                    self.DEFAULT_TEXT, show_line_numbers=True, language="python"
+                    self.DEFAULT_CODE, show_line_numbers=True, language="python"
                 )
                 with HorizontalGroup():
                     yield Button("submit")
@@ -63,7 +30,8 @@ def solution():
         yield Footer()
 
     def action_show_modal(self):
-        self.app.push_screen(PassScreen())
+        code = self.query_one(TextArea)
+        self.app.push_screen(ResultModal(code.text))
 
 
 # if __name__ == "__main__":
