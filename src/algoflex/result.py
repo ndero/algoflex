2d177c0a097758c1be34c83d3af49aaa6c1c26c3
ndero <jnderoh@gmail.com>
2023-06-05 06:20:38 +0300

    ch: refine result modal UI

--- src/algoflex/result.py
+++ src/algoflex/result.py
@@ -1,47 +1,54 @@
 from textual.screen import ModalScreen
-from textual.widgets import Log, Button, TextArea
-from textual.containers import VerticalGroup, Center
-import textwrap
+from textual.widgets import Log, Button, TextArea, RichLog
+from textual.containers import VerticalScroll, Center
+
 import tempfile
 import subprocess
 import os
-from textual.app import App
 
 
 class ResultModal(ModalScreen):
     BINDINGS = [("q", "dismiss", "dismiss")]
     DEFAULT_CSS = """
     ResultModal {
+        &>* {
+            max-width: 90;
+        }
         align: center middle;
-        width: 50;
+        RichLog {
+            width: 1fr;
+            height: 12;
+            padding: 1 0;
+            padding-left: 2;
+            overflow-x: auto;
+            background: $boost;
+        }
     }
     """
-    TEST_CODE = textwrap.dedent(
-        """
-        def run_tests():
-            test_cases = [
-                (([2, 7, 11, 15], 9), [0, 1]),
-                (([3, 2, 4], 6), [1, 2]),
-                (([3, 3], 6), [0, 1]),
-                (([1, 2, 3], 4), [0, 2]),
-            ]
-            passed = 0
-            for i, (inputs, expected) in enumerate(test_cases):
-                try:
-                    result = solution(*inputs)
-                    if result == expected:
-                        print(f"Test case {i+1} passed ✅")
-                        passed += 1
-                    else:
-                        print(f"Test case {i+1} failed ❌ — got {result}, expected {expected}")
-                except Exception as e:
-                    print(f"Test case {i+1} error ❌ — {e}")
-            print(f"\\nPassed {passed}/{len(test_cases)} test cases.")
+    TEST_CODE = """
+def run_tests():
+    test_cases = [
+        (([2, 7, 11, 15], 9), [0, 1]),
+        (([3, 2, 4], 6), [1, 2]),
+        (([3, 3], 6), [0, 1]),
+        (([1, 2, 3], 4), [0, 2]),
+    ]
+    passed = 0
+    for i, (inputs, expected) in enumerate(test_cases):
+        try:
+            result = solution(*inputs)
+            if result == expected:
+                print(f"Test case {i+1} passed ✅")
+                passed += 1
+            else:
+                print(f"Test case {i+1} failed ❌ — got {result}, expected {expected}")
+        except Exception as e:
+            print(f"Test case {i+1} error ❌ — {e}")
+    print(f"\\nPassed {passed}/{len(test_cases)} test cases.")
 
-        if __name__ == "__main__":
-            run_tests()
-        """
-    )
+if __name__ == "__main__":
+    run_tests()
+    """
 
     def __init__(self, code):
         super().__init__()
@@ -51,14 +58,11 @@ class ResultModal(ModalScreen):
         self.run_user_code()
 
     def compose(self):
-        with VerticalGroup(id="code"):
-            yield Log(highlight=True)
-            yield TextArea("Passed!")
-            yield Button("Ok")
+        yield RichLog(highlight=True)
 
     def run_user_code(self):
         user_code = self.code.strip()
-        output_log = self.query_one(Log)
+        output_log = self.query_one(RichLog)
         full_code = f"{user_code}\n\n{self.TEST_CODE}"
         with tempfile.NamedTemporaryFile(
             delete=False, suffix=".py", mode="w"
