d8c5d508e8e717bc460e67fed4225347220ad7e4
ndero <jnderoh@gmail.com>
2023-06-28 00:40:53 +0300

    ch: receive problem description from parent class and functionality for cancel action

--- src/algoflex/attempt.py
+++ src/algoflex/attempt.py
@@ -1,6 +1,6 @@
 from textual.app import App
 from textual.widgets import TextArea, Button, Markdown, Footer
-from home import ProblemScreen, TitleScreen
+from custom_widgets import TitleScreen, ProblemScreen
 from result import ResultModal
 from textual.containers import Vertical, Horizontal
 from textual.screen import ModalScreen, Screen
@@ -8,7 +8,10 @@ from textual.binding import Binding
 
 
 class AttemptScreen(Screen):
-    BINDINGS = [Binding("s", "submit", "submit", tooltip="submit your solution")]
+    BINDINGS = [
+        Binding("s", "submit", "submit", tooltip="submit your solution"),
+        Binding("c", "cancel", "cancel", tooltip="Go to home"),
+    ]
     DEFAULT_CSS = """
     ProblemScreen {
         margin: 0 1;
@@ -26,10 +29,14 @@ def solution(nums, target):
         lookup[num] = i
 """
 
+    def __init__(self, problem):
+        super().__init__()
+        self.problem = problem
+
     def compose(self):
         yield TitleScreen()
         with Horizontal():
-            yield ProblemScreen()
+            yield ProblemScreen(self.problem)
             with Vertical():
                 yield TextArea(
                     self.DEFAULT_CODE,
@@ -43,3 +50,6 @@ def solution(nums, target):
     def action_submit(self):
         code = self.query_one(TextArea)
         self.app.push_screen(ResultModal(code.text))
+
+    def action_cancel(self):
+        self.dismiss()
