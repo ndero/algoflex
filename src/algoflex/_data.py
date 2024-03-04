90ab718a0d29e9ad31c18ad1f25d26cb5cd6b5e2
ndero <jnderoh@gmail.com>
2024-03-04 17:09:13 +0300

    ch: add problem - course schedule

--- src/algoflex/_data.py
+++ src/algoflex/_data.py
@@ -880,4 +880,32 @@ explanation: rob houses at indices 0 and 2
             [[[i for i in range(0, 100_000, 100)]], 25_000_000],
         ],
     },
+    28: {
+        "markdown": """
+### Course schedule
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+
+Given the number of courses `n` and  a `prerequisites` array, return the ordering of courses you should take to finish all courses.
+
+If there are many valid answers, return any of them. If it is impossible to finish all courses, return an empty array.
+
+### Example
+```
+input: n = 2, prerequisites = [[1, 0]]
+output: [0, 1]
+
+input: n = 4, prerequisites = [[1, 0], [2, 0], [3, 1], [3, 2]]
+output: [0, 1, 2, 3] or [0, 2, 1, 3]
+```
+""",
+        "title": "Course schedule",
+        "difficulty": "Medium",
+        "test_cases": [
+            [[2, [[1, 0]]], [0, 1]],
+            [[4, [[1, 0], [2, 0], [3, 1], [3, 2]]], [0, 1, 2, 3]],
+            [[1, []], [0]],
+        ],
+    },
 }
