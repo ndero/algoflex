b59beb1b66bb06a48ebf06d7cf7b5170a5f8c256
ndero <jnderoh@gmail.com>
2024-03-05 10:22:33 +0300

    ch: add problem - minimum height trees

--- src/algoflex/_data.py
+++ src/algoflex/_data.py
@@ -908,4 +908,29 @@ output: [0, 1, 2, 3] or [0, 2, 1, 3]
             [[1, []], [0]],
         ],
     },
+    29: {
+        "markdown": """
+### Minimum height trees (MHTs)
+A tree is an undirected graph in which any two vertices are connected by exactly one path. In other words, any connected graph without simple cycles is a tree.
+
+Given a tree of `n` nodes labelled from 0 to n - 1, and an array of n - 1 edges where edges[i] = [ai, bi] indicates that there is an undirected edge between the two nodes ai and bi in the tree, you can choose any node of the tree as the root. When you select a node x as the root, the result tree has height h. Among all possible rooted trees, those with minimum height (i.e. min(h)) are called minimum height trees (MHTs).
+
+Return a list of all MHTs' root labels. You can return the answer in any order. The height of a rooted tree is the number of edges on the longest downward path between the root and a leaf.
+
+### Example
+```
+input: n = 4, edges = [[1, 0], [1, 2], [1, 3]]
+output: [1]
+
+input: n = 6, prerequisites = [[3, 0], [3, 1], [3, 2], [3, 4], [5, 4]]
+output: [3, 4]
+```
+""",
+        "title": "Minimum height trees",
+        "difficulty": "Medium",
+        "test_cases": [
+            [[4, [[1, 0], [1, 2], [1, 3]]], [1]],
+            [[6, [[3, 0], [3, 1], [3, 2], [3, 4], [5, 4]]], [3, 4]],
+        ],
+    },
 }
