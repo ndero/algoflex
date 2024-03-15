21676210f0fc07eb01deb1c3b9e8eed8048cde48
ndero <jnderoh@gmail.com>
2024-03-15 18:35:13 +0300

    ch: add problem - cheapest flight with at most k stops

--- src/algoflex/_data.py
+++ src/algoflex/_data.py
@@ -1022,4 +1022,66 @@ output: ""
             [[["flower", "flow", ""]], ""],
         ],
     },
+    32: {
+        "markdown": """
+### Cheapest flight with at most k stops
+There are n cities connected by some number of flights. You are given an array flights where flights[i] = [fromi, toi, pricei] indicates that there is a flight from city fromi to city toi with cost pricei. You are also given three integers src, dest, and k.
+
+Return the cheapest price from src to dst with at most k stops. If there is no such route, return -1.
+
+### Example
+```
+inputs: n = 4, flights = [[0, 1, 100], [1, 2, 100], [2, 0, 100], [1, 3, 600], [2, 3, 200]], src = 0, dest = 3, k = 1
+output: 700
+explanation: the cheapest flight from city 0 to 3 with at most one stop is:
+0 -> 1 at a cost of 100
+1 -> 3 at a cost of 600
+Total cost 700
+```
+""",
+        "title": "Cheapest flight with at most k stops",
+        "difficulty": "Medium",
+        "test_cases": [
+            [
+                [
+                    4,
+                    [[0, 1, 100], [1, 2, 100], [2, 0, 100], [1, 3, 600], [2, 3, 200]],
+                    0,
+                    3,
+                    1,
+                ],
+                700,
+            ],
+            [
+                [
+                    4,
+                    [[0, 1, 100], [1, 2, 100], [2, 0, 100], [1, 3, 600], [2, 3, 200]],
+                    0,
+                    3,
+                    2,
+                ],
+                400,
+            ],
+            [
+                [
+                    3,
+                    [[0, 1, 100], [1, 2, 100], [0, 2, 500]],
+                    0,
+                    2,
+                    1,
+                ],
+                200,
+            ],
+            [
+                [
+                    3,
+                    [[0, 1, 100], [1, 2, 100], [0, 2, 500]],
+                    0,
+                    2,
+                    0,
+                ],
+                500,
+            ],
+        ],
+    },
 }
