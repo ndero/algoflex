8cae02e7b87dfa1990c238ee4d423c54edd61303
ndero <jnderoh@gmail.com>
2024-03-18 22:07:27 +0300

    ch: add problem - network delay time 1

--- src/algoflex/_data.py
+++ src/algoflex/_data.py
@@ -1084,4 +1084,25 @@ Total cost 700
             ],
         ],
     },
+    33: {
+        "markdown": """
+### Network delay time I
+You are given a network of n nodes, labeled from 1 to n. You are also given times, a list of travel times as directed edges times[i] = (ui, vi, wi), where ui is the source node, vi is the target node, and wi is the time it takes for a signal to travel from source to target. We will send a signal from a given node k.
+
+Return the minimum time it takes for all the n nodes to receive the signal. If it is impossible for all the n nodes to receive the signal, return -1.
+
+### Example
+```
+inputs: times = [[2, 1, 1], [2, 3, 1], [3, 4, 1]], n = 4, src = 2
+output: 2
+```
+""",
+        "title": "Network delay time I",
+        "difficulty": "Medium",
+        "test_cases": [
+            [[[[2, 1, 1], [2, 3, 1], [3, 4, 1]], 4, 2], 2],
+            [[[[1, 2, 1]], 2, 1], 1],
+            [[[[1, 2, 1]], 4, 2], -1],
+        ],
+    },
 }
