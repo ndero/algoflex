336e2b960828d21e2cdaee2834cf082c00ef11f0
ndero <jnderoh@gmail.com>
2024-04-15 21:36:26 +0300

    ch: add problem - job scheduling

--- src/algoflex/_data.py
+++ src/algoflex/_data.py
@@ -1375,4 +1375,30 @@ output: (220, [0, 1, 1])
             [[5, [10, 20, 30], [60, 100, 120]], (0, [0, 0, 0])],
         ],
     },
+    43: {
+        "markdown": """
+### Job scheduling
+You have n jobs, where every job is scheduled to be done from startTime[i] to endTime[i], obtaining a profit of profit[i].
+
+You're given the startTime, endTime and profit arrays, return the maximum profit you can take such that there are no two jobs in the subset with overlapping time range.
+
+If you choose a job that ends at time X you will be able to start another job that starts at time X.
+
+### Example
+```
+inputs:
+    start_time = [1, 2, 3, 3],
+    end_time = [3, 4, 5, 6],
+    profit = [50, 10, 40, 70]
+
+output: 120
+```
+""",
+        "title": "0/1 knapsack",
+        "difficulty": "Easy",
+        "test_cases": [
+            [[[1, 2, 3, 3], [3, 4, 5, 6], [50, 10, 40, 70]], 120],
+            [[[1, 1, 1], [2, 3, 4], [5, 6, 4]], 6],
+        ],
+    },
 }
