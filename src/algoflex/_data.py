988d91fccd76d9f68ebebef929381422a7746ad6
ndero <jnderoh@gmail.com>
2024-04-29 22:41:35 +0300

    ch: add problem - min cost tickets

--- src/algoflex/_data.py
+++ src/algoflex/_data.py
@@ -1452,4 +1452,36 @@ explanation: two 5 coins and one 1 coin.
             [[[1, 2, 5, 10, 20, 50], 77], 4],
         ],
     },
+    46: {
+        "markdown": """
+### Min cost tickets
+You have planned some train traveling one year in advance. The days of the year in which you will travel are given as an integer array days. Each day is an integer from 1 to 365. Train tickets are sold in three different ways:
+
+- a 1-day pass is sold for costs[0] dollars,
+- a 7-day pass is sold for costs[1] dollars
+- a 30-day pass is sold for costs[2] dollars.
+
+The passes allow that many days of consecutive travel. Return the minimum number of dollars you need to travel every day in the given list of days.
+
+### Example
+```
+inputs:
+  days = [1, 4, 6, 7, 8, 20]
+  costs = [2, 7, 15]
+
+output: 11
+```
+""",
+        "title": "Min cost tickets",
+        "difficulty": "Medium",
+        "test_cases": [
+            [[[1, 4, 6, 7, 8, 20], [2, 7, 15]], 11],
+            [[[1, 2, 3, 4, 5, 6, 7], [2, 7, 15]], 7],
+            [[[i for i in range(1, 31)], [2, 7, 15]], 15],
+            [[[1, 4, 6], [2, 7, 15]], 6],
+            [[[5, 6, 7, 8, 9, 10, 11], [2, 7, 15]], 7],
+            [[[5, 6, 7, 8, 9, 10, 11, 210, 211, 212, 213, 365], [2, 7, 15]], 16],
+            [[[i for i in range(1, 366)], [2, 7, 15]], 190],
+        ],
+    },
 }
