ce8977696a95da610325cea79c4a8b60b95b2bde
ndero <jnderoh@gmail.com>
2024-11-05 21:52:07 +0300

    ch: add problem - house robber III

--- src/algoflex/_data.py
+++ src/algoflex/_data.py
@@ -1625,4 +1625,29 @@ output: 6
             [[10_000], 134235101],
         ],
     },
+    49: {
+        "markdown": """
+### House robber III
+The thief has found himself a new place for his thievery again. There is only one entrance to this area, called root. Besides the root, each house has one and only one parent house.
+
+After a tour, the smart thief realized that all houses in this place form a binary tree. It will automatically contact the police if two directly-linked houses were broken into on the same night.
+
+Given the root of the binary tree, return the maximum amount of money the thief can rob without alerting the police.
+
+### Example
+```
+input: [6, 3, 9, None, 5, 4, 9]
+output: 24
+```
+""",
+        "test_cases": f"""
+{tree_code}
+root = array_to_tree([6, 3, 9, None, 5, 4, 9])
+test_cases = [
+    [[root], 24],
+]
+""",
+        "title": "House robber III",
+        "difficulty": "Medium",
+    },
 }
