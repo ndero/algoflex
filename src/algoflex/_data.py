150b509f2584bcec83aa18db19d0c360879476b6
ndero <jnderoh@gmail.com>
2025-01-09 23:27:52 +0300

    ch: add problem - boolean tree

--- src/algoflex/_data.py
+++ src/algoflex/_data.py
@@ -1758,4 +1758,42 @@ test_cases = [
         "title": "Same binary tree",
         "difficulty": "Medium",
     },
+    53: {
+        "markdown": """
+### Boolean tree
+You are given the root of a full binary tree with the following properties:
+- Leaf nodes have either the value 0 or 1, where 0 represents False and 1 represents True.
+- Non-leaf nodes have either the value 2 or 3, where 2 represents the boolean OR and 3 represents the boolean AND.
+
+The evaluation of a node is as follows:
+- If the node is a leaf node, the evaluation is the value of the node, i.e. True or False.
+- Otherwise, evaluate the node's two children and apply the boolean operation of its value with the children's evaluations.
+
+Return the boolean result of evaluating the root node.
+
+A full binary tree is a binary tree where each node has either 0 or 2 children.
+
+A leaf node is a node that has zero children.
+
+### Example
+```
+input: [2, 1, 3, None, None, 0, 1]
+output: True
+
+input: [0]
+output: False
+```
+""",
+        "test_cases": f"""
+{binary_tree}
+t1 = array_to_tree([2, 1, 3, None, None, 0, 1])
+t2 = array_to_tree([0])
+test_cases = [
+    [[t1], True],
+    [[t2], False],
+]
+""",
+        "title": "Boolean binary tree",
+        "difficulty": "Medium",
+    },
 }
