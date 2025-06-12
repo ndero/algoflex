5d84db944b98d99e20259bd853247e759068cd66
ndero <jnderoh@gmail.com>
2025-06-12 23:48:23 +0300

    ch: add problem - merge sort

--- src/algoflex/_data.py
+++ src/algoflex/_data.py
@@ -2238,4 +2238,24 @@ test_cases = [
         "title": "Value in array",
         "difficulty": "Easy",
     },
+    70: {
+        "markdown": """
+### Merge sort
+Given an array of integers, use merge sort algorithm to return an array of all the integers sorted in non decreasing order.
+
+### Example
+```
+input: [8, 2, 4, 9, 12, 18, 16]
+output: [2, 4, 8, 9, 12, 13, 16, 18]
+```
+""",
+        "test_cases": """
+test_cases = [
+    [[[8, 2, 4, 9, 12, 18, 16]], [2, 4, 8, 9, 12, 13, 16, 18]],
+    [[[i for i in range(100_000, -1, -1)]], [i for i in range(100_001)]],
+]
+""",
+        "title": "Merge sort",
+        "difficulty": "Easy",
+    },
 }
