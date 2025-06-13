3a8222011e6766fd262948fddb2504dc10bef74c
ndero <jnderoh@gmail.com>
2025-06-13 19:51:16 +0300

    ch: add problem - heap sort

--- src/algoflex/_data.py
+++ src/algoflex/_data.py
@@ -2258,4 +2258,24 @@ test_cases = [
         "title": "Merge sort",
         "difficulty": "Easy",
     },
+    71: {
+        "markdown": """
+### Heap sort
+Given an array of integers, use heap sort algorithm to return an array of all the integers sorted in non decreasing order.
+
+### Example
+```
+input: [8, 2, 4, 9, 12, 18, 16]
+output: [2, 4, 8, 9, 12, 13, 16, 18]
+```
+""",
+        "test_cases": """
+test_cases = [
+    [[[8, 2, 4, 9, 12, 18, 16]], [2, 4, 8, 9, 12, 13, 16, 18]],
+    [[[i for i in range(100_000, -1, -1)]], [i for i in range(100_001)]],
+]
+""",
+        "title": "Heap sort",
+        "difficulty": "Easy",
+    },
 }
