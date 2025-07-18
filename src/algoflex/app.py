14ec7b47cfc94179723daa48bb2cfcb80f5b58a4
ndero <jnderoh@gmail.com>
2025-07-18 16:37:22 +0300

    ft: allow users to run with `algoflex` command

--- src/algoflex/app.py
+++ src/algoflex/app.py
@@ -1,28 +1,10 @@
-from textual.app import App
-from textual.binding import Binding
-from home import HomeScreen
-from attempt import AttemptScreen
+from algoflex.home import HomeScreen
 
 
-class AlgoFlex(App):
-    MODES = {"home": HomeScreen, "attempt": AttemptScreen}
-    DEFAULT_MODE = "home"
-    BINDINGS = [
-        Binding("h", "app.switch_mode('home')", "home", tooltip="Go to home"),
-        Binding(
-            "a",
-            "app.switch_mode('attempt')",
-            "attempt",
-            tooltip="Attempt this question",
-        ),
-    ]
+def main():
+    app = HomeScreen()
+    app.run()
 
-    def check_action(self, action, parameters):
-        # disable footer switcher for current screen
-        if (
-            action == "switch_mode"
-            and parameters
-            and self.current_mode == parameters[0]
-        ):
-            return
-        return True
+
+if __name__ == "__main__":
+    main()
