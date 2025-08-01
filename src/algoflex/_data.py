2372a3cf63eac873c5006166b9c8a54e3593b7a5
ndero <jnderoh@gmail.com>
2025-08-01 17:31:09 +0300

    ch:  add problem - insertion sort

--- src/algoflex/_data.py
+++ src/algoflex/_data.py
@@ -1437,6 +1437,26 @@ test_cases = [
         "title": "Bubble sort",
         "difficulty": "Easy",
     },
+    45: {
+        "markdown": """
+### Insertion sort
+Given an array of integers, use insertion sort algorithm to return an array of all the integers sorted in non decreasing order.
+
+### Example
+```
+input: [8, 2, 4, 9, 12, 18, 16, 13]
+output: [2, 4, 8, 9, 12, 13, 16, 18]
+```
+""",
+        "test_cases": """
+test_cases = [
+    [[[8, 2, 4, 9, 12, 18, 16, 13]], [2, 4, 8, 9, 12, 13, 16, 18]],
+    [[[i for i in range(100_000, -1, -1)]], [i for i in range(100_001)]],
+]
+""",
+        "title": "Insertion sort",
+        "difficulty": "Easy",
+    },
     44: {
         "markdown": """
 ### Smaller to the right
