dc035c1eb7bd78bec78369c2a6442e8407d2a713
ndero <jnderoh@gmail.com>
2025-08-02 22:13:51 +0300

    ch: don't exit on failing test. Continue to run the rest of the tests and give a count of total failing tests at the end.

--- src/algoflex/result.py
+++ src/algoflex/result.py
@@ -41,19 +41,24 @@ def display(params):
     return [truncate(param) for param in params]
 
 def run_tests():
+    total, passed = len(test_cases), 0
     for i, [inputs, expected] in enumerate(test_cases):
         try:
             result = solution(*inputs)
             if result == expected:
                 print(f"[green][b]~[/] test case {i+1} passed![/]")
+                passed += 1
             else:
                 print(f"[red][b]x[/] test case {i+1} failed![/] \\n\\t[b]inputs[/]: {display(inputs)}\\n\\t[b]got[/]: [red]{result}[/]\\n\\t[b]expected[/]: [green]{expected}[/]")
-                return 1
         except Exception as e:
             print(f"[red][b]x[/] test case {i+1} error![/]\\n\\t[b]error[/]: {e}\\n\\t[b]inputs[/]: {display(inputs)}")
             return 1
-    print(f"\\nPassed!")
-    return 0
+    if passed == total:
+        print(f"\\n{passed}/{total} passed!")
+        return 0
+    if passed < total:
+        print(f"\\n {total - passed} failing.")
+    return 1
 
 if __name__ == "__main__":
     sys.exit(run_tests())
