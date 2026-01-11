75e1b99af27a59f368d8413e33cffb7fb0e4e9dc
ndero <jnderoh@gmail.com>
2026-01-11 18:49:59 +0300

    ch: attempt screen css

--- src/algoflex/attempt.py
+++ src/algoflex/attempt.py
@@ -1,7 +1,7 @@
 from textual import on
 from textual.app import App
 from textual.widgets import TextArea, Footer, TabbedContent, Button
-from textual.containers import Horizontal, Vertical, VerticalGroup
+from textual.containers import Horizontal, Vertical
 from textual.screen import Screen
 from textual.binding import Binding
 from algoflex.custom_widgets import Title, Problem
@@ -17,7 +17,6 @@ stats = get_db()
 
 class AttemptScreen(Screen):
     BINDINGS = [
-        Binding("s", "submit", "submit", tooltip="submit your solution"),
         Binding("b", "back", "back", tooltip="Go to home"),
     ]
     DEFAULT_CSS = """
@@ -25,10 +24,23 @@ class AttemptScreen(Screen):
         Problem {
             margin: 0 1;
             height: 1fr;
+            width: 1fr;
+        }
+        TabbedContent {
+            width: 1fr;
         }
     }
     TextArea {
         margin-right: 1;
+    }
+    Vertical {
+        Horizontal {
+            height: 4;
+            align: center middle;
+            background: $boost;
+            border-top: hkey $background;
+            margin-right: 1;
+        }
     }
     """
 
@@ -48,41 +60,43 @@ class AttemptScreen(Screen):
         with Horizontal():
             yield Problem(description)
             with TabbedContent("Attempt", "Recent Solution", "Saved Solution"):
-                yield TextArea(
-                    code,
-                    show_line_numbers=True,
-                    language="python",
-                    compact=True,
-                    tab_behavior="indent",
-                )
+                with Vertical():
+                    yield TextArea(
+                        code,
+                        show_line_numbers=True,
+                        language="python",
+                        compact=True,
+                        tab_behavior="indent",
+                    )
+                    with Horizontal():
+                        yield Button(id="submit", label="Submit", flat=True)
+                        yield Button(id="save", label="Save", flat=True)
                 yield TextArea(
                     recent_code,
                     show_line_numbers=True,
                     language="python",
                     compact=True,
-                    # tab_behavior="indent",
                     read_only=True,
                     placeholder="# Recent correct submitted solution will be shown here.",
                 )
-                with VerticalGroup():
-                    yield TextArea(
-                        saved_code,
-                        show_line_numbers=True,
-                        language="python",
-                        compact=True,
-                        tab_behavior="indent",
-                        placeholder="# You can save a solution here for future reference",
-                    )
-                    yield Button("Default", id="save")
-
+                yield TextArea(
+                    saved_code,
+                    show_line_numbers=True,
+                    language="python",
+                    compact=True,
+                    read_only=True,
+                    placeholder="# Your saved solution will appear here",
+                )
         yield Footer()
 
     @on(Button.Pressed, "#save")
     def save_code(self):
-        code = self.query(TextArea)[1]
+        code = self.query_one(TextArea)
         stats.upsert({"saved_code": code.text}, KV.problem_id == self.problem_id)
+        self.notify("Saved - can be accessed in saved solution tab")
 
-    def action_submit(self):
+    @on(Button.Pressed, "#submit")
+    def submit_code(self):
         code = self.query_one(TextArea)
         elapsed = monotonic() - self.test_time
         self.app.push_screen(ResultModal(self.problem_id, code.text, elapsed))
