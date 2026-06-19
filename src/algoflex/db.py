6ece1925ae6bc2e23509069e5b3b0447c69d60f2
ndero <jnderoh@gmail.com>
2026-06-19 09:57:17 +0300

    ch: add draft table

--- src/algoflex/db.py
+++ src/algoflex/db.py
@@ -1,8 +1,10 @@
-from tinydb import TinyDB
+from tinydb import TinyDB, Query
 from platformdirs import user_data_dir
 from pathlib import Path
+from time import time 
 
 _db_instance = None
+KV = Query()
 
 
 def get_db():
@@ -13,3 +15,23 @@ def get_db():
         path = Path(app_data_dir, "attempts.json")
         _db_instance = TinyDB(path=path)
     return _db_instance
+
+attempts = get_db()
+drafts = get_db().table("drafts")
+
+def save_draft(problem_id, code, elapsed):
+    drafts.upsert(
+        {
+            "problem_id": problem_id,
+            "code": code, 
+            "elapsed": elapsed,
+            "updated_at": time(),
+        },
+        KV.problem_id == problem_id,
+    )
+
+def load_draft(problem_id):
+    return drafts.get(KV.problem_id == problem_id)
+
+def delete_draft(problem_id):
+    drafts.remove(KV.problem_id == problem_id)
