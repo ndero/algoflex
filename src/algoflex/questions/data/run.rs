1c8401b39c9eb42ace5b3f5c888d740ca98b86d4
ndero <jnderoh@gmail.com>
2026-08-17 17:01:00 +0300

    ft: change rust runner to a macros to deal with different shapes of function and testcases inputs.

--- src/algoflex/questions/data/run.rs
+++ src/algoflex/questions/data/run.rs
@@ -1,49 +1,56 @@
-use std::panic::{catch_unwind, AssertUnwindSafe};
+macro_rules! run_tests {
+    ($test_cases:expr, |$input:ident| $call:expr) => {{
+        use std::panic::{catch_unwind, AssertUnwindSafe};
 
-pub fn run_tests<F>(
-    func: F,
-    test_cases: &[(Vec<&str>, i64)],
-) -> i32
-where
-    F: Fn(&[&str]) -> i64,
-{
-    for (i, (args, expected)) in test_cases.iter().enumerate() {
-        match catch_unwind(AssertUnwindSafe(|| func(args))) {
-            Ok(result) if result == *expected => {
-                println!("[b]✓[/] test case {}\t[green]... ok[/]", i + 1);
+        let test_cases = $test_cases;
+
+        'tests: {
+            for (i, (input, expected)) in test_cases.iter().enumerate() {
+                match catch_unwind(AssertUnwindSafe(|| {
+                    let $input = input;
+                    $call
+                })) {
+                    Ok(result) if result == *expected => {
+                        println!(
+                            "[b]✓[/] test case {}\t[green]... ok[/]",
+                            i + 1
+                        );
+                    }
+
+                    Ok(result) => {
+                        println!(
+                            "[b]x[/] test case {}\t[red]... FAIL[/]\n\
+                             \t[b]args[/]: {:?}\n\
+                             \t[b]got[/]: [red]{:?}[/]\n\
+                             \t[b]expected[/]: [green]{:?}[/]",
+                            i + 1,
+                            input,
+                            result,
+                            expected
+                        );
+
+                        break 'tests 1;
+                    }
+
+                    Err(error) => {
+                        println!(
+                            "[b]x[/] test case {}\t[red]... ERROR[/]\n\
+                             \t[b]error[/]: {}",
+                            i + 1,
+                            format_panic(error)
+                        );
+
+                        break 'tests 1;
+                    }
+                }
             }
 
-            Ok(result) => {
-                println!(
-                    "[b]x[/] test case {}\t[red]... FAIL[/]\n\
-                     \t[b]args[/]: {:?}\n\
-                     \t[b]got[/]: [red]{}[/]\n\
-                     \t[b]expected[/]: [green]{}[/]",
-                    i + 1,
-                    args,
-                    result,
-                    expected
-                );
-
-                return 1;
-            }
-
-            Err(error) => {
-                println!(
-                    "[b]x[/] test case {}\t[red]... ERROR[/]\n\
-                     \t[b]error[/]: {}",
-                    i + 1,
-                    format_panic(error)
-                );
-
-                return 1;
-            }
+            println!("\n{} passed!", test_cases.len());
+            0
         }
-    }
-
-    println!("\n{} passed!", test_cases.len());
-    0
+    }};
 }
+
 
 fn format_panic(error: Box<dyn std::any::Any + Send>) -> String {
     if let Some(message) = error.downcast_ref::<&str>() {
