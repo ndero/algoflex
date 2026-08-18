7d986dcb52cce79b1cda3e411382b3e8af44f152
ndero <jnderoh@gmail.com>
2026-08-18 16:03:46 +0300

    ch: add problem - integer to roman

---
+++ src/algoflex/questions/data/13/problem.md
@@ -0,0 +1,22 @@
+### Roman numerals
+Convert a given integer, `n`,  to its equivalent roman numerals for `0 < n < 4000`.
+
+|Decimal | 1000 | 900 | 500 | 400 | 100 |90|
+|--------|------|-----|-----|-----|----|---|
+|Roman | M | CM | D | CD | C |XC|
+
+|Decimal| 50| 40 | 10| 9 | 5 | 4 | 1 |
+|-------|---|----|---|---|---|---|---|
+|Roman  | L | XL | X | IX| V | IV| I|
+
+
+### Example
+```
+input: 4
+output = 'IV'
+```
+
+```
+input: 23
+output = 'XXIII'
+```
---
+++ src/algoflex/questions/data/13/python_tests.py
@@ -0,0 +1,25 @@
+import sys
+
+test_cases = [
+    [(4,), "IV"],
+    [(23,), "XXIII"],
+    [(768,), "DCCLXVIII"],
+    [(1,), "I"],
+    [(3999,), "MMMCMXCIX"],
+    [(369,), "CCCLXIX"],
+    [(1318,), "MCCCXVIII"],
+    [(1089,), "MLXXXIX"],
+    [(2424,), "MMCDXXIV"],
+    [(999,), "CMXCIX"],
+    # Edge cases
+    [(3,), "III"],  # Smallest with repetition
+    [(9,), "IX"],  # Subtractive notation
+    [(40,), "XL"],  # Tens subtractive
+    [(90,), "XC"],  # Tens subtractive
+    [(400,), "CD"],  # Hundreds subtractive
+    [(900,), "CM"],  # Hundreds subtractive
+    [(3888,), "MMMDCCCLXXXVIII"],  # Longest roman numeral
+]
+
+if __name__ == "__main__":
+    sys.exit(run_python_tests(int_to_roman, test_cases))  # type: ignore  # noqa: F821
