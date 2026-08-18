3a0fd99b2df7ecbfb5be5366a2b9ac7fe2b86efd
ndero <jnderoh@gmail.com>
2026-08-18 10:56:44 +0300

    ch: add problem - minimum in rotated array

---
+++ src/algoflex/questions/data/8/problem.md
@@ -0,0 +1,10 @@
+### Min in rotated array
+Given a sorted (ascending order) but rotated array `nums`, return the minimum element of this array. You must write an algorithm that runs in **O(log n)** time.
+
+> an example of rotating an array. If `[0, 1, 2, 4, 5, 6, 7]` is rotated 4 times it becomes `[4, 5, 6, 7, 0, 1, 2]`.
+
+### Example
+```
+arr = [4, 5, 6, 7, 0, 1, 2]
+output = 0
+```
---
+++ src/algoflex/questions/data/8/rust_tests.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let test_cases = vec![
+        ((vec![4, 5, 6, 7, 0, 1, 2],), 0),
+        ((vec![16, 23, 43, 55, -7, -4, 3, 5, 9, 15],), -7),
+        (((36..1_000_000).step_by(10).collect(),), 36),
+        (({
+            let mut v: Vec<i32> = (-10..1_000_000).step_by(10).collect();
+            v.extend((-1_000_000..-10).step_by(10));
+            v
+        },), -1_000_000),
+        ((vec![2],), 2),
+        ((vec![134, 140, 147, 156, 160, 164, 166, 166, 170, 183, 184, 192, -9, -4, 1, 20, 51, 54, 54, 56, 67, 75, 80, 88, 92, 93, 96, 105, 115, 127],), -9),
+        // Edge cases
+        ((vec![1],), 1),                                      // Single element
+        ((vec![1, 2],), 1),                                   // Two elements, not rotated
+        ((vec![2, 1],), 1),                                   // Two elements, rotated
+        ((vec![1, 2, 3, 4, 5],), 1),                          // Not rotated
+        ((vec![5, 1, 2, 3, 4],), 1),                          // Rotated once
+        ((vec![3, 3, 3, 1, 3],), 1),                          // Duplicates with min
+        ((vec![3, 1, 3, 3, 3],), 1),                          // Duplicates with min at start
+    ];
+
+    std::process::exit(
+        run_tests!(&test_cases, |input| min_rotated_arr(&input.0))
+    );
+}
