ae70c1a2ed945d8a6ae626e2ad0a2eeb56acabdd
ndero <jnderoh@gmail.com>
2026-09-09 11:32:15 +0300

    ch: change rust signature to accept an array of 3 items [u, v, w]

--- src/algoflex/questions/data/52/rust_tests.rs
+++ src/algoflex/questions/data/52/rust_tests.rs
@@ -1,23 +1,30 @@
 fn main() {
-    let mut network: Vec<Vec<i32>> = Vec::new();
-    for i in 1..10 {
-        network.push(vec![i, i + 1, i * 100]);
-    }
-    for i in (1..10).step_by(2) {
-        network.push(vec![i, i + 2, 100]);
-    }
-    network.push(vec![10, 1, 10_000]);
+    let network: Vec<[i32; 3]> = {
+        let mut network = Vec::new();
 
-    let test_cases: Vec<((Vec<Vec<i32>>, i32, i32), i32)> = vec![
-        ((vec![vec![2, 1, 1], vec![2, 3, 1], vec![3, 4, 1]], 4, 2), 2),
-        ((vec![vec![1, 2, 1]], 2, 1), 1),
-        ((vec![vec![1, 2, 1]], 4, 2), -1),
-        ((vec![vec![1, 2, 6]], 2, 1), 6),
-        ((vec![vec![1, 2, 6]], 2, 2), -1),
+        for i in 1..10 {
+            network.push([i, i + 1, i * 100]);
+        }
+
+        for i in (1..10).step_by(2) {
+            network.push([i, i + 2, 100]);
+        }
+
+        network.push([10, 1, 10_000]);
+
+        network
+    };
+
+    let test_cases: Vec<((Vec<[i32; 3]>, i32, i32), i32)> = vec![
+        ((vec![[2, 1, 1], [2, 3, 1], [3, 4, 1]], 4, 2), 2),
+        ((vec![[1, 2, 1]], 2, 1), 1),
+        ((vec![[1, 2, 1]], 4, 2), -1),
+        ((vec![[1, 2, 6]], 2, 1), 6),
+        ((vec![[1, 2, 6]], 2, 2), -1),
         ((network.clone(), 11, 1), 1300),
-        ((network.clone(), 11, 2), 11400),
+        ((network.clone(), 11, 2), 11_400),
         ((network.clone(), 11, 11), -1),
-        ((network.clone(), 11, 5), 11500),
+        ((network.clone(), 11, 5), 11_500),
         // edge case
         ((vec![], 1, 1), 0),
     ];
